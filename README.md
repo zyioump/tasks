5cfe68addbeccf5d667738ec9156991173423f14
Christophe NOUCHET <nouchet.christophe@gmail.com>
2017-01-22 23:17:08 +0100

    Initial commit again

---
+++ README.md
@@ -0,0 +1,19 @@
+# Open Path View Tasks
+
+## Todo
+
+Write this software
+
+## License
+
+Copyright (C) 2017 Open Path View, Maison Du Libre <br />
+This program is free software; you can redistribute it and/or modify  <br />
+it under the terms of the GNU General Public License as published by  <br />
+the Free Software Foundation; either version 3 of the License, or  <br />
+(at your option) any later version.  <br />
+This program is distributed in the hope that it will be useful,  <br />
+but WITHOUT ANY WARRANTY; without even the implied warranty of  <br />
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  <br />
+GNU General Public License for more details.  <br />
+You should have received a copy of the GNU General Public License along  <br />
+with this program. If not, see <http://www.gnu.org/licenses/>.  <br />
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2017 Open Path View, Maison Du Libre
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+# Merci Sam & Max : http://sametmax.com/creer-un-setup-py-et-mettre-sa-bibliotheque-python-en-ligne-sur-pypi/
+
+import opv_directorymanager
+
+setup(
+    name='opv_tasks',
+    version='0.0.1',
+    packages=find_packages(),
+    author="Christophe NOUCHET",
+    author_email="team@openpathview.fr",
+    description="Open Path View Tasks",
+    long_description=open('README.md').read(),
+    install_requires=[],
+    # Active la prise en compte du fichier MANIFEST.in
+    include_package_data=False,
+    url='https://github.com/OpenPathView/OPV_Tasks',
+
+    scripts=[],
+
+    license="GPL3",
+)
